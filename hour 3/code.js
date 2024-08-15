32c3875bc327ec33d08718285deae6a6ce18f5fd
ALE123456789101112 <146310872+ALE123456789101112@users.noreply.github.com>
2024-08-14 20:16:01 -0400

    Add files via upload
    Added teamPoints array to track scores, and set up a round-robin tournament with a new table button. Also, made a playGame function to simulate matches with home-field advantage

---
+++ hour 3/code.js
@@ -0,0 +1,74 @@
+var index = 0;
+var teams = [];
+var winRate = [];
+var tempTeam = "";
+var tempWin = 0;
+var teamPoints=[];
+
+onEvent("confirm", "click", function( ) {
+  tempTeam = getText("newTeamInput");
+  tempWin = getText("win%Input");
+  if (tempTeam == ""||tempWin==undefined||tempWin>100||tempWin<0) {
+    setText("teamList", "Please enter a name for your team");
+    setText("win%List", "Please enter a number from 0 to 100");
+  } else {
+    appendItem(teams, getText("newTeamInput"));
+    appendItem(winRate, getText("win%Input"));
+    appendItem(teamPoints, 0);
+    updateScreen();
+  }
+});
+
+
+onEvent("previousTeam", "click", function( ) {
+  if (index > 0) {
+    index = index - 1;
+  } //when the left button is clicked and the index is greater than 0, then its value decreases by one, 
+  //therefore the position of the list goes one back
+  updateScreen();
+});
+
+onEvent("nextTeam", "click", function( ) {
+  if (index < teams.length - 1) {
+    index = index + 1;
+  } //when the right button is clicked and the index is less than 1 subtracted from the length of the list,
+  //then its value increases by one, therefore the position of the list goes one forward
+  updateScreen();
+});
+
+onEvent("table", "click", function( ) {
+  for (var k = 0; k < teams.length; k++) {
+    for (var i = 0; i < teams.length; i++) {
+      if (k != i) {
+        var matchResult = playGame(k, i);
+        console.log(matchResult);
+      }
+    }
+  }
+  console.log(teamPoints);
+});
+
+
+
+function updateScreen() {
+    setProperty("teamList", "text", teams[index]);
+    setProperty("page", "text", index+1);
+    setProperty("win%List", "text", winRate[index]+"% win rate");
+    setProperty("newTeamInput", "text", "");
+    setProperty("win%Input", "text", "");
+}
+
+function playGame(team1Index, team2Index) {
+  if (winRate[team1Index] > winRate[team2Index] - randomNumber(1, 10)) {
+    teamPoints[team1Index] = teamPoints[team1Index] + 3;
+    return (teams[team1Index] + " wins against "+teams[team2Index]+" at home!");
+  } else if (winRate[team1Index] < winRate[team2Index] - randomNumber(1, 10)) {
+    teamPoints[team2Index] = teamPoints[team2Index] + 3;
+    return ((teams[team1Index] + " loses against ")+teams[team2Index]+" at home!");
+  } else if ((winRate[team1Index] == winRate[team2Index] - randomNumber(1, 10))) {
+    teamPoints[team1Index] = teamPoints[team1Index] + 1;
+    teamPoints[team2Index] = teamPoints[team2Index] + 1;
+    return ((teams[team1Index] + " draws to ")+teams[team2Index]+" at home!");
+  }
+  //subtracting a random number between 1 and 10 from the win% of the away team because home teams have an advantage
+}
